19fd3b1d896962eb16d79fec90fbc22f1de1c923
Ryan1729 <Ryan1729@gmail.com>
2022-01-21 00:06:00 -0700

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
 use std::env;
 use std::fs::OpenOptions;
 use std::io::{Read, Write, Seek, SeekFrom};
@@ -19,49 +8,48 @@ use regex::Regex;
 fn main() {
     let mut args = env::args();
 
-	args.next();
+    args.next();
 
-	if let (Some(filename), Some(regex), Some(capture_indicator)) = (args.next(), args.next(), args.next()) {
-		let capture_num = capture_indicator.parse::<usize>().unwrap();
+    if let (Some(filename), Some(regex), Some(capture_indicator)) = (args.next(), args.next(), args.next()) {
+        let capture_num = capture_indicator.parse::<usize>().unwrap();
 
-		let re = Regex::new(&regex).unwrap();
+        let re = Regex::new(&regex).unwrap();
 
-		let mut file = OpenOptions::new().read(true).write(true).open(&filename).unwrap();
-		let mut file_string = String::new();
-		file.read_to_string(&mut file_string).unwrap();
+        let mut file = OpenOptions::new().read(true).write(true).open(&filename).unwrap();
+        let mut file_string = String::new();
+        file.read_to_string(&mut file_string).unwrap();
 
-		if let Some(cap) = re.captures_iter(&file_string).next() {
-			let version_str = &cap[capture_num];
-			let mut version = version_str.parse::<usize>().unwrap();
-			version += 1;
+        if let Some(cap) = re.captures_iter(&file_string).next() {
+            let version_str = &cap[capture_num];
+            let mut version = version_str.parse::<usize>().unwrap();
+            version += 1;
 
-			let mut replacement_string = String::new();
+            let mut replacement_string = String::new();
 
-			for i in 1..capture_num {
-				replacement_string.push_str(&format!("${{{}}}", i));
-			}
+            for i in 1..capture_num {
+                replacement_string.push_str(&format!("${{{}}}", i));
+            }
 
-			replacement_string.push_str(&format!("{}", version));
+            replacement_string.push_str(&format!("{}", version));
 
-			if cap.len() - 1 > capture_num { 
-				for i in (capture_num + 1)..cap.len() {
-					replacement_string.push_str(&format!("${{{}}}", i));
-				}
-			}
+            if cap.len() - 1 > capture_num { 
+                for i in (capture_num + 1)..cap.len() {
+                    replacement_string.push_str(&format!("${{{}}}", i));
+                }
+            }
 
-			let replacement_str : &str = &replacement_string;
-			
-			let replacement_result = re.replace(&file_string, replacement_str);
-			
-			file.seek(SeekFrom::Start(0)).unwrap();
-			file.write_all(replacement_result.as_bytes()).unwrap();
+            let replacement_str : &str = &replacement_string;
+            
+            let replacement_result = re.replace(&file_string, replacement_str);
+            
+            file.seek(SeekFrom::Start(0)).unwrap();
+            file.write_all(replacement_result.as_bytes()).unwrap();
 
-			println!("overwrote {}", filename);
-		} else {
-			println!("No captures found!")
-		}
-	} else {
-		println!("usage: increment_version_number filename regex capture_indicator")
-	}
- 
+            println!("overwrote {}", filename);
+        } else {
+            println!("No captures found!")
+        }
+    } else {
+        println!("usage: increment_version_number filename regex capture_indicator")
+    } 
 }
